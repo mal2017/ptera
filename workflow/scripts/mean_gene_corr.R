c50f8aef3f0eb4a7a55cdf522a579a2ee9ca3130
Matthew Lawlor <mal456@amarel2.amarel.rutgers.edu>
2022-12-01 10:39:51 -0500

    genexgene correlation

--- workflow/scripts/mean_gene_corr.R
+++ workflow/scripts/mean_gene_corr.R
@@ -2,16 +2,23 @@ library(DescTools)
 library(tidyverse)
 library(corrr)
 
+#fls <- c("subworkflows/dgrp_coex/results/linear_models/female/0/gene_x_gene.corrr.rds", "subworkflows/dgrp_coex/results/linear_models/female/1/gene_x_gene.corrr.rds","subworkflows/dgrp_coex/results/linear_models/female/2/gene_x_gene.corrr.rds")
+
 fls <- snakemake@input
 
-df <- map(fls, read_rds) %>%
-  map_df(stretch, .id = "rep")
+df <- map(fls, read_rds)
 
-res <- df %>%
-  mutate(fishz = FisherZ(r) ) %>%
+#df <- map(df,~{.x[1:10000,1:10001]}) %>% map_df(stretch, .id = "rep")
+
+df <- map_df(df,stretch, .id = "rep")
+
+res <- df %>% mutate(fishz = FisherZ(r))
+
+res <- res %>%
   group_by(x, y) %>%
-  summarise(mean_fishz = mean(fishz), .groups = "drop") %>%
-  mutate(mean_r=FisherZInv(mean_fishz)) %>%
+  summarise(mean_fishz = mean(fishz), .groups = "drop")
+
+res <-  mutate(res,mean_r=FisherZInv(mean_fishz)) %>%
   dplyr::select(x, y, r = mean_r)
 
 res <- retract(res,x,y,r) 
